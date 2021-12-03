693158a40fcebc6d92d35d49a402ed2d6e2d1d84
Auryn Engel <auryn.engel@sap.com>
2021-12-03 10:04:29 +0100

    day 3

---
+++ day_3/src/main.rs
@@ -0,0 +1,147 @@
+use std::fs;
+
+
+use itertools::*;
+fn main() {
+    let filename = "./input.txt";
+    run_for_file(filename);
+}
+
+fn run_for_file(filename: &str) {
+    println!("In file {}", filename);
+    let arr = parse_file(filename);
+    let position = part_1(&arr);
+    println!("part 1 {}", position);
+    let position_2 = part_2(&arr);
+    println!("part 2 {}", position_2);
+}
+
+fn parse_file(filename: &str) -> Vec<Vec<i32>> {
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    return  contents
+        .split("\n")
+        .map(|it|  transform(it))
+        .collect();
+}
+
+fn part_1(arr: &Vec<Vec<i32>>) -> i32 {
+
+    let mut vec = arr.clone();
+    let init_vec: Vec<i32> = vec![0; vec[0].len()];
+    println!("init {}", init_vec.iter().join(""));
+    let v = vec.iter().fold(init_vec,|acc, line| {
+        let result = acc.iter().zip(line.iter()).map(|(a,b)| {
+            // println!("A: {}, B: {}", a , b);
+            a+b
+        }).collect_vec();
+        println!("line {}", result.iter().join(""));
+        result
+    });
+    
+    let mut gamma_vec = Vec::new();
+    let mut epsilon_vec = Vec::new();
+    for i in 0..v.len() {
+        // println!("{}", v[i]);
+        gamma_vec.push( if v[i]>((arr.len() as i32) - v[i]) {1} else {0});
+        epsilon_vec.push( if v[i]<((arr.len() as i32) - v[i]) {1} else {0});
+        // println!("{}", v[i]);
+    }
+
+    println!("gamma_vec {}", gamma_vec.iter().join(""));
+    println!("epsilon_vec {}", epsilon_vec.iter().join(""));
+    let gamma_rate =isize::from_str_radix(&gamma_vec.iter().join("").to_owned(), 2).unwrap() as i32;
+    let epsilon_rate =isize::from_str_radix(&epsilon_vec.iter().join("").to_owned(), 2).unwrap() as i32;
+    
+    return gamma_rate * epsilon_rate;
+}
+
+fn part_2(arr: &Vec<Vec<i32>>) -> i32 {
+    let mut oxygen_vec = arr.clone();
+    for i in 0..oxygen_vec[0].len() {
+        let mut oxygen_vec_numbers : Vec<i32> = Vec::new();
+        for i in 0..oxygen_vec[0].len() {
+            oxygen_vec_numbers.push(0);
+            for j in 0..oxygen_vec.len() {
+                oxygen_vec_numbers[i] += oxygen_vec[j][i];
+            }
+        }
+        oxygen_vec = oxygen_vec.iter()
+        .filter(|line| {
+            if oxygen_vec_numbers[i] >= ((oxygen_vec.len() as i32) - oxygen_vec_numbers[i]) {
+                line[i] == 1
+            } else {
+                line[i] == 0
+            }
+        })
+        .cloned()
+        .collect::<Vec<Vec<i32>>>();
+        if oxygen_vec.len() == 1 {
+            break;
+        }
+    }
+
+    let mut co2_vec = arr.clone();
+    for i in 0..co2_vec[0].len() {
+        let mut co2_vec_numbers : Vec<i32> = Vec::new();
+        for i in 0..co2_vec[0].len() {
+            co2_vec_numbers.push(0);
+            for j in 0..co2_vec.len() {
+                co2_vec_numbers[i] += co2_vec[j][i];
+            }
+        }
+        co2_vec = co2_vec.iter()
+        .filter(|line| {
+            if co2_vec_numbers[i] < ((co2_vec.len() as i32) - co2_vec_numbers[i]) {
+                return line[i] == 1;
+            } else {
+                return line[i] == 0;
+            }
+        })
+        .cloned()
+        .collect::<Vec<Vec<i32>>>();
+        
+        if co2_vec.len() == 1 {
+            break;
+        }
+    }
+    
+    let oxygen_rate =isize::from_str_radix(&oxygen_vec[0].iter().join("").to_owned(), 2).unwrap() as i32;
+    let co2_rate =isize::from_str_radix(&co2_vec[0].iter().join("").to_owned(), 2).unwrap() as i32;
+    return oxygen_rate*co2_rate;
+}
+
+fn transform(line: &str) -> Vec<i32> {
+    return line
+        .chars()
+        .map(|it| it as i32 - 0x30)
+        .collect::<Vec<i32>>();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_input_part_one() {
+        let arr = parse_file("./exampleInput.txt");
+        assert_eq!(part_1(&arr), 198);
+    }
+
+    #[test]
+    fn test_example_input_part_two() {
+        let arr = parse_file("./exampleInput.txt");
+        assert_eq!(part_2(&arr), 230);
+    }
+
+    #[test]
+    fn test_input_part_one() {
+        let arr = parse_file("./input.txt");
+        assert_eq!(part_1(&arr), 3882564);
+    }
+
+    #[test]
+    fn test_input_part_two() {
+        let arr = parse_file("./input.txt");
+        assert_eq!(part_2(&arr), 3385170);
+    }
+}
